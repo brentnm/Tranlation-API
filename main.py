c7133e4b152efb92f0e13bdcdd22878fe936af3e
Nathan Brent <brentnm@dukes.jmu.edu>
2021-04-21 23:34:17 -0400

    corrected trailing space

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from translate import Translator
 translator= Translator(to_lang=input("Type in 'es' for spanish, 'pt' for portuguese, or 'zh' for chinese: "))
 while True:
    translation = translator.translate(input("What do you want to translate?: "))
-   print (translation) 
+   print (translation)
