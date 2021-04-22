513c13fd8b99729633138b850b20598fa4b0edce
Nathan Brent <brentnm@dukes.jmu.edu>
2021-04-21 23:33:12 -0400

    improved user inputs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from translate import Translator
-translator= Translator(to_lang="es")
+translator= Translator(to_lang=input("Type in 'es' for spanish, 'pt' for portuguese, or 'zh' for chinese: "))
 while True:
    translation = translator.translate(input("What do you want to translate?: "))
    print (translation) 
